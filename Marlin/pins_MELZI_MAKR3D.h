a3819a7ea46792d209c039faf3a2a28e14831654
Scott Lahteine <sourcetree@thinkyhead.com>
2016-07-01 15:31:15 -0700

    Handle MELZI variants in Sanguinololu 11

--- Marlin/pins_MELZI_MAKR3D.h
+++ Marlin/pins_MELZI_MAKR3D.h
@@ -24,8 +24,6 @@
  * Melzi with ATmega1284 (MaKr3d version) pin assignments
  */
 
-#undef MOTHERBOARD
-#define MOTHERBOARD BOARD_MELZI
 #define SANGUINOLOLU_V_1_2
 
 #if defined(__AVR_ATmega1284P__)
